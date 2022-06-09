82d3ad545f3cfd068ee39c52e48c189cb39f9a9a
auskov77 <auskov77@yandex.ru>
2022-06-10 00:04:00 +0300

    tests services

--- src/test/java/ru/itsjava/services/IOServiceImplTest.java
+++ src/test/java/ru/itsjava/services/IOServiceImplTest.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.itsjava.services.IOService;
-import ru.itsjava.services.IOServiceImpl;
 
 import java.io.ByteArrayInputStream;
 
