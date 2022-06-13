c6e2f7395ba561d3e6ac74286b74013911850480
auskov77 <auskov77@yandex.ru>
2022-06-14 00:27:37 +0300

    tests services

--- src/test/java/ru/itsjava/services/AppServiceImplTest.java
+++ src/test/java/ru/itsjava/services/AppServiceImplTest.java
@@ -2,17 +2,21 @@ package ru.itsjava.services;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.itsjava.dao.CustomerDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import ru.itsjava.dao.PetDao;
 import ru.itsjava.domain.Customer;
 import ru.itsjava.domain.Pet;
 
-import java.util.Arrays;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @DisplayName("Тест методов класса AppService")
 @SpringBootTest
@@ -20,73 +24,71 @@ public class AppServiceImplTest {
     private static final String DEFAULT_NAME = "Запашный";
     private static final String DEFAULT_EMAIL = "zapashniy@mail.ru";
     private static final String DEFAULT_ANIMAL = "Tiger";
-    public static final Pet DEFAULT_PET_FIRST_ID = new Pet(1L, "yard");
+    public static final Pet DEFAULT_PET = new Pet(1L, "yard");
+    private static final Customer DEFAULT_CUSTOMER = new Customer(1L, DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_ANIMAL, DEFAULT_PET);
 
-//    @Configuration
-//    static class AppServiceConfiguration {
-//
-//        @Bean
-//        public IOService ioService() {
-//            IOServiceImpl mockIOService = Mockito.mock(IOServiceImpl.class);
-//            final int numMenu = 1;
-//            when(mockIOService.input()).thenReturn(String.valueOf(numMenu));
-//            return mockIOService;
-//        }
-//
-////        @Bean
-////        public CustomerService customerService() {
-////            CustomerServiceImpl mockCustomerService = Mockito.mock(CustomerServiceImpl.class);
-////            when(mockCustomerService.insert()).thenReturn(new Customer());
-////            return new CustomerServiceImpl();
-////        }
-////
-////        @Bean
-////        public PetDao petDao() {
-////            return new PetDaoImpl();
-////        }
-//
-////        @Bean
-////        public AppService appService(CustomerService customerService, IOService ioService, PetDao petDao) {
-////            return new AppServiceImpl(customerService, ioService, petDao);
-////        }
-//    }
+    @Configuration
+    static class ConfigurationAppServiceImpl {
+        public static final String PRIVET = "1";
 
-    @Autowired
-    private IOService ioService;
+        @Bean
+        public CustomerService customerService() {
+            CustomerService mockCustomerService = Mockito.mock(CustomerService.class);
+            when(mockCustomerService.findAll()).thenReturn(List.of(DEFAULT_CUSTOMER));
+            return mockCustomerService;
+        }
 
-    @Autowired
-    private CustomerService customerService;
+        @Bean
+        public IOService ioService() {
+            IOService mockIOService = Mockito.mock(IOService.class);
+            when(mockIOService.input()).thenReturn(PRIVET);
+            return mockIOService;
+        }
+
+        @Bean
+        public PetDao petDao() {
+            PetDao mockPetDao = Mockito.mock(PetDao.class);
+            when(mockPetDao.findAll()).thenReturn(List.of(DEFAULT_PET));
+            return mockPetDao;
+        }
+
+        @Bean
+        public AppService appService(IOService ioService, CustomerService customerService, PetDao petDao) {
+            return new AppServiceImpl(customerService, ioService, petDao);
+        }
+    }
 
     @Autowired
-    private CustomerDao customerDao;
-
-    @Autowired
-    private PetDao petDao;
+    private AppService appService;
 
 //    @DisplayName("Тест метода start")
 //    @Test
-//    void start() {
+//    void shouldHaveCorrectMethodStart() {
+//
 //    }
 
     @DisplayName("Тест метода printAllCustomers")
     @Test
-    void printAllCustomers() {
-        List<Customer> expectedCustomerList = customerService.findAll();
+    void shouldHaveCorrectMethodPrintAllCustomers() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        appService.printAllCustomers();
 
-        assertEquals(Arrays.toString(expectedCustomerList.toArray()),
-                "[Customer(id=1, name=Ivanov, email=ivanov1@yandex.ru, animal=cat, pet=Pet(id=1, breed=yard)), Customer(id=2, name=Petrov, email=petrov11@yandex.ru, animal=dog, pet=Pet(id=2, breed=fighting)), Customer(id=3, name=Запашный, email=zapashniy@mail.ru, animal=Tiger, pet=Pet(id=1, breed=yard))]");
+        assertEquals("Список всех владельцев животных [Customer(id=1, name=Запашный, email=zapashniy@mail.ru, animal=Tiger, pet=Pet(id=1, breed=yard))]\r\n",
+                outputStream.toString());
     }
 
     @DisplayName("Тест метода insertCustomer")
     @Test
     public void shouldHaveCorrectMethodInsertCustomer() {
-        Customer expectedCustomer = new Customer(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_ANIMAL, DEFAULT_PET_FIRST_ID);
-        long idExpectedCustomerFromDB = customerDao.insert(expectedCustomer);
-        Customer customerById = customerDao.findById(idExpectedCustomerFromDB);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        appService.insertCustomer();
 
-        assertAll(()->assertEquals(customerById.getName(), expectedCustomer.getName()),
-                ()->assertEquals(customerById.getEmail(),expectedCustomer.getEmail()),
-                ()->assertEquals(customerById.getAnimal(), expectedCustomer.getAnimal()));
-
+        assertEquals("Введите владельца\r\n" +
+                "Введите ФИО\r\n" +
+                "Введите электронный адрес\r\n" +
+                "Введите животное\r\n" +
+                "Введите породу животного\r\n", outputStream.toString());
     }
 }
--- src/test/java/ru/itsjava/services/CustomerServiceImplTest.java
+++ src/test/java/ru/itsjava/services/CustomerServiceImplTest.java
@@ -2,8 +2,11 @@ package ru.itsjava.services;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import ru.itsjava.dao.CustomerDao;
 import ru.itsjava.domain.Customer;
 import ru.itsjava.domain.Pet;
@@ -11,6 +14,7 @@ import ru.itsjava.domain.Pet;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @DisplayName("Тест методов класса CustomerServiceImpl")
 @SpringBootTest
@@ -18,28 +22,43 @@ class CustomerServiceImplTest {
     private static final String DEFAULT_NAME = "Запашный";
     private static final String DEFAULT_EMAIL = "zapashniy@mail.ru";
     private static final String DEFAULT_ANIMAL = "Tiger";
-    public static final Pet DEFAULT_PET_FIRST_ID = new Pet(1L, "yard");
+    public static final Pet DEFAULT_PET = new Pet(1L, "yard");
+    public static final Customer DEFAULT_CUSTOMER = new Customer(1L, DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_ANIMAL, DEFAULT_PET);
+
+    @Configuration
+    static class ConfigurationCustomerServiceImpl {
+
+        @Bean
+        public CustomerDao customerDao() {
+            CustomerDao mockCustomerDao = Mockito.mock(CustomerDao.class);
+            when(mockCustomerDao.findAll()).thenReturn(List.of(DEFAULT_CUSTOMER));
+            return mockCustomerDao;
+        }
+
+        @Bean
+        public CustomerService customerService(){
+            return new CustomerServiceImpl(customerDao());
+        }
+    }
 
     @Autowired
-    private CustomerDao customerDao;
+    private CustomerService customerService;
 
     @DisplayName("Тест метода insert")
     @Test
     public void shouldHaveCorrectMethodInsert() {
-        Customer expectedCustomer = new Customer(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_ANIMAL, DEFAULT_PET_FIRST_ID);
-        long idExpectedCustomerFromDB = customerDao.insert(expectedCustomer);
-        Customer customerById = customerDao.findById(idExpectedCustomerFromDB);
-
-        assertAll(()->assertEquals(customerById.getName(), expectedCustomer.getName()),
-                ()->assertEquals(customerById.getEmail(),expectedCustomer.getEmail()),
-                ()->assertEquals(customerById.getAnimal(), expectedCustomer.getAnimal()));
+        customerService.insert(DEFAULT_CUSTOMER);
+        Customer expectedCustomer = new Customer("Vtoroy", "vtoroy@mail.ru", "maus", DEFAULT_PET);
+        customerService.insert(expectedCustomer);
+        assertAll(()->assertEquals(DEFAULT_CUSTOMER.getName(), DEFAULT_NAME),
+                ()->assertEquals(DEFAULT_CUSTOMER.getEmail(), DEFAULT_EMAIL),
+                ()->assertEquals(DEFAULT_CUSTOMER.getAnimal(), DEFAULT_ANIMAL),
+                ()->assertEquals(DEFAULT_CUSTOMER.getId(), 1));
     }
 
     @DisplayName("Тест метода findAll")
     @Test
     void shouldHaveCorrectMethodFindAll() {
-        List<Customer> customerList = customerDao.findAll();
-
-        assertEquals(3, customerList.size());
+        assertEquals(1, customerService.findAll().size());
     }
 }
